7ab9e02f7397446064fcc078a1212ef058a273be
craig.sirois <carlsagan.work@gmail.com>
2023-02-06 12:50:30 -0800

    Added models

--- routes/nova.js
+++ routes/nova.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
    res.render('nova', { name: 'Nova'})
-   res.send('Hello Nova! Nodemon?');
+   // res.send('Hello Nova! Nodemon?');
 });
 
 module.exports = router;
