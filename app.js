f56df187a787e176d718ca4fb7c556c3b29bd820
craig.sirois <carlsagan.work@gmail.com>
2023-03-13 09:17:39 -0700

    Complete Part 4

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var novaRouter = require('./routes/nova');
-
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
 
 var app = express();
@@ -35,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/nova', novaRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
